5933e59bd3830b6061ba9d2d76f9a7ffda1c1b67
Alexandr Sergeevich <aaa@aaa>
2019-12-02 11:33:08 +0300

    everything is broken

--- Cube.java
+++ Cube.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Cube {
     public static void main(String[] args) {
-        
+        double x = 0;
+        double y = 0;
+        double z = 0;
+        coord(x,y,z);
+        R3Vector v1 = new R3Vector();
+        coord(x,y,z);
+        R3Vector v2 = new R3Vector();
+        coord(x,y,z);
+        R3Vector v3 = new R3Vector();
+        coord(x,y,z);
+        R3Vector v4 = new R3Vector();
+        Facet a = new Facet(v1, v2, v3, v4);
+    }
+    public static void coord(double x, double y, double z){
+        Scanner in = new Scanner(System.in);
+        x = in.nextDouble();
+        y = in.nextDouble();
+        z = in.nextDouble();
     }
 }
--- Facet.java
+++ Facet.java
@@ -1,11 +1,21 @@
-public class Facet {
+import java.awt.*;
+
+public class Facet extends R3Vector{
     private R3Vector[] vertex;
     //Color color;
     public Facet(R3Vector v1, R3Vector v2, R3Vector v3, R3Vector v4){
         R3Vector[] vertex = {v1, v2, v3, v4};
+        vertex[0] = v1;
+        vertex[1] = v2;
+        vertex[2] = v3;
+        vertex[3] = v4;
+        System.out.println(v1 + " - "+ v2);
+        System.out.println(v2 + " - "+ v3);
+        System.out.println(v3 + " - "+ v4);
+        System.out.println(v4 + " - "+ v1);
     }
     public void print(){
     //закручиваем рассчет против часовой стрелки
-       
+
     }
 }
